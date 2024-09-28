c453e39199fe06ebdceabe9c48ef259404de5efa
Guilhere Britz Videira <gbritzv@gmail.com>
2024-09-27 23:58:26 -0300

    feat: create initial projects to work: core, app, tests

---
+++ src/SimpleFactory.App/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleFactory.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
